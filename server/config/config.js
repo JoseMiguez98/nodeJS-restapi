8a13e6f1a7c605d575dd404024c6e7bc74f6c0c8
JoseMiguez98 <josemiguez184@gmail.com>
2020-11-21 18:00:14 -0300

    Rest server - added MONGO_URI env variable

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ const ENV = require('./constants');
 */
 
 if(process.env.NODE_ENV === ENV.PROD) {
-  process.env.URLDB = 'mongodb+srv://JoseMiguez98:Fsociety505;@cluster0.qibwv.mongodb.net/coffee-udemy?retryWrites=true&w=majority'
+  process.env.URLDB = process.env.MONGO_URI;
 } else {
   process.env.URLDB = 'mongodb://localhost:27017/coffee-udemy';
 }
